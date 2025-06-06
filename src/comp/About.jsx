5ceea8db0fcb2d847b83d9104d913d46b9891061
Abhay prasad <153746890+prasad-abhay@users.noreply.github.com>
2025-06-06 22:49:39 +0530

    Update About.jsx

--- src/comp/About.jsx
+++ src/comp/About.jsx
@@ -2,10 +2,7 @@ export default function About(){
     return (
         <>
         <h1>About</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos delectus enim voluptates ea. Su
-            scipit esse reiciendis, aut asperiores voluptas provident neque cumque ipsum fugiat porro expli
-            cabo eaque perspiciatis quidem dolore quod optio totam. Rem doloremque distinctio, aliquid et
-             fugit quos est, expedita corporis aperiam consequuntur consectetur voluptates? Quas, inventore numquam!
+        <p>This a TodoList App biuld using React.js by Abhay Prasad on 6th june 2025.
         </p>
         </>
     )
